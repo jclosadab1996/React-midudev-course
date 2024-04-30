2a405edc1d5b083e553d9f3f372750eae88b0ed7
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-04-29 23:25:07 -0500

    new

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,43 +1,41 @@
-import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 
 const users = [
   {
-    userName: 'midudev',
-    name: 'Miguel Ángel Durán',
-    isFollowing: true
+    userName: "juancalosada22",
+    name: "Juan Camilo Losada B",
+    isFollowing: true,
   },
   {
-    userName: 'pheralb',
-    name: 'Pablo H.',
-    isFollowing: false
+    userName: "pheralb",
+    name: "Pablo H.",
+    isFollowing: false,
   },
   {
-    userName: 'PacoHdezs',
-    name: 'Paco Hdez',
-    isFollowing: true
+    userName: "PacoHdezs",
+    name: "Paco Hdez",
+    isFollowing: true,
   },
   {
-    userName: 'TMChein',
-    name: 'Tomas',
-    isFollowing: false
-  }
-]
+    userName: "TMChein",
+    name: "Tomas",
+    isFollowing: false,
+  },
+];
 
-export function App () {
+export function App() {
   return (
-    <section className='App'>
-      {
-        users.map(({ userName, name, isFollowing }) => (
-          <TwitterFollowCard
-            key={userName}
-            userName={userName}
-            initialIsFollowing={isFollowing}
-          >
-            {name}
-          </TwitterFollowCard>
-        ))
-      }
+    <section className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </section>
-  )
+  );
 }
